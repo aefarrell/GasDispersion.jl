a641d7108426aae299d659e88edada82d1556848
aefarrell <allanf@protonmail.com>
2023-01-25 13:24:03 -0700

    Updated correlations and corrections for near and far field
    Updated correlations to use the versions from TSCREEN, updated the near-field to handle large \alpha, where the interpolation and near-field overlap, also updated the far-field region to decay like 1/x^2 starting from the last concentration in the interpolation region.
    
    Updated the back end of the plume, instead of guessing at a shape it is just a box since nothing is given in the Britter-McQuaid model.

--- src/models/britter_mcquaid_plume.jl
+++ src/models/britter_mcquaid_plume.jl
@@ -3,28 +3,124 @@ struct BritterMcQuaidPlume <: PlumeModel end
 struct BritterMcQuaidPlumeSolution <: Plume
     scenario::Scenario
     model::Symbol
-    ρⱼ::Number # jet density
+    c₀::Number # initial concentration, kg/m³
     T′::Number # temperature correction
     D::Number  # critical length
     lb::Number # plume dimension parameter, m
-    itp::Interpolations.Extrapolation
+    itp::Interpolations.GriddedInterpolation
+    xnf::Number # near field distance
+    xff::Number # far field distance
+    A::Number  # far field constant
 end
 
-Britter_McQuaid_correlations = Dict(
-    0.010 => ( αs=[-1.0, -0.7, -0.29, -0.2, 1.0],
-               βs=[2.25, 2.25, 2.45, 2.45, 1.83]),
-    0.005 => ( αs=[-1.0, -0.67, -0.28, -0.15, 1.0],
-               βs=[2.4, 2.4, 2.63, 2.63, 2.07]),
-    0.020 => ( αs=[-1.0, -0.69, -0.31, -0.16, 1.0],
-               βs=[2.08, 2.08, 2.25, 2.25, 1.62]),
-    0.002 => ( αs=[-1.0, -0.69, -0.25, -0.13, 1.0],
-               βs=[2.6, 2.6, 2.77, 2.77, 2.21]),
-    0.100 => ( αs=[-1.0, -0.55, -0.14, 1.0],
-               βs=[1.75, 1.75, 1.85, 1.28]),
-    0.050 => ( αs=[-1.0, -0.68, -0.29, -0.18, 1.0],
-               βs=[1.92, 1.92, 2.06, 2.06, 1.4]),
-)
+# Britter-McQuaid plume correlations
+# as digitzed in TSCREEN
+function _bm_pl_c_1(α)
+    # corresponds to (Cm/C0) = 0.10
+    if α ≤ -0.55
+        return 1.75
+    elseif -0.55 < α ≤ -0.14
+        return 0.24*α + 1.88
+    elseif -0.14 < α ≤ 1.0
+        return -0.5*α + 1.78
+    else
+        @warn "α= $α is out of range"
+        return -0.5*α + 1.78
+    end
+end
 
+function _bm_pl_c_05(α)
+    # corresponds to (Cm/C0) = 0.05
+    if α ≤ -0.68
+        return 1.92
+    elseif -0.68 < α ≤ -0.29
+        return 0.36*α + 2.16
+    elseif -0.29 < α ≤ -0.18
+        return 2.06
+    elseif -0.18 < α ≤ 1.0
+        return -0.56*α + 1.96
+    else
+        @warn "α= $α is out of range"
+        return -0.56*α + 1.96
+    end
+end
+
+function _bm_pl_c_02(α)
+    # corresponds to (Cm/C0) = 0.02
+    if α ≤ -0.69
+        return 2.08
+    elseif -0.69 < α ≤ -0.31
+        return 0.45*α + 2.39
+    elseif -0.31 < α ≤ -0.16
+        return 2.25
+    elseif -0.16 < α ≤ 1.0
+        return -0.54*α + 2.16
+    else
+        @warn "α= $α is out of range"
+        return -0.54*α + 2.16
+    end
+end
+
+function _bm_pl_c_01(α)
+    # corresponds to (Cm/C0) = 0.01
+    if α ≤ -0.70
+        return 2.25
+    elseif -0.70 < α ≤ -0.29
+        return 0.49*α + 2.59
+    elseif -0.29 < α ≤ -0.20
+        return 2.45
+    elseif -0.20 < α ≤ 1.0
+        return -0.52*α + 2.35
+    else
+        @warn "α= $α is out of range"
+        return -0.52*α + 2.35
+    end
+end
+
+function _bm_pl_c_005(α)
+    # corresponds to (Cm/C0) = 0.005
+    if α ≤ -0.67
+        return 2.40
+    elseif -0.67 < α ≤ -0.28
+        return 0.59*α + 2.80
+    elseif -0.28 < α ≤ -0.15
+        return 2.63
+    elseif -0.15 < α ≤ 1.0
+        return -0.48*α + 2.56
+    else
+        @warn "α= $α is out of range"
+        return -0.48*α + 2.56
+    end
+end
+
+function _bm_pl_c_002(α)
+    # corresponds to (Cm/C0) = 0.002
+    if α ≤ -0.69
+        return 2.60
+    elseif -0.69 < α ≤ -0.25
+        return 0.39*α + 2.87
+    elseif -0.25 < α ≤ -0.13
+        return 2.77
+    elseif -0.13 < α ≤ 1.0
+        return -0.50*α + 2.71
+    else
+        @warn "α= $α is out of range"
+        return -0.50*α + 2.71
+    end
+end
+
+function _bm_pl_c(α)
+    concs = [0.10, 0.05, 0.02, 0.01, 0.005, 0.002]
+    βs = [
+            _bm_pl_c_1(α),   # (C_m/C_0) = 0.1
+            _bm_pl_c_05(α),  # (C_m/C_0) = 0.05
+            _bm_pl_c_02(α),  # (C_m/C_0) = 0.02
+            _bm_pl_c_01(α),  # (C_m/C_0) = 0.01
+            _bm_pl_c_005(α), # (C_m/C_0) = 0.005
+            _bm_pl_c_002(α)  # (C_m/C_0) = 0.002
+    ]
+    return concs, βs
+end
 
 """
     plume(scenario::Scenario, BritterMcQuaidPlume)
@@ -38,8 +134,8 @@ other coordinates are ignored.
 """
 function plume(scenario::Scenario, ::Type{BritterMcQuaidPlume})
 
-    Q = _mass_rate(scenario)
-    h = _release_height(scenario)
+    Q = _release_flowrate(scenario)
+    ṁ = _mass_rate(scenario)
     ρⱼ = _release_density(scenario)
     Tᵣ = _release_temperature(scenario)
 
@@ -47,62 +143,74 @@ function plume(scenario::Scenario, ::Type{BritterMcQuaidPlume})
     ρₐ = _atmosphere_density(scenario)
     Tₐ = _atmosphere_temperature(scenario)
 
-    # Setting up the Britter-McQuaid curves
-    britter_interps = [ ]
-    concs = sort(collect(keys(Britter_McQuaid_correlations)), rev=true)
-
-    for conc in concs
-        αs, βs = Britter_McQuaid_correlations[conc]
-        f = extrapolate(interpolate((αs,), βs, Gridded(Linear())), Line())
-        push!(britter_interps, (c=conc, it=f))
-    end
+    # initial concentration
+    c₀ = ṁ/Q
 
     # relative density
     g = 9.80616  # gravity, m/s^2
     gₒ = g * ((ρⱼ - ρₐ)/ ρₐ)
 
     # critical distance
-    Vr = Q/ρⱼ # volumetric release rate
-    D = √(Vr/u₁₀)
+    D = √(Q/u₁₀)
 
     # temperature correction
-    T′ = Tₐ/Tᵣ
+    T′ = Tᵣ/Tₐ
 
     # correlation parameter
-    α = 0.2*log10( gₒ^2 * Vr * u₁₀^-5 )
+    α = 0.2*log10( gₒ^2 * Q * u₁₀^-5 )
 
     # setting up correlation for constant α
     # calculates the points for the linear interpolation
-    concs = [ elem.c for elem in britter_interps ]
-    βs = [ elem.it(α) for elem in britter_interps ]
+    concs, βs = _bm_pl_c(α)
 
-    # linear interpolation between the short distance correlation
-    # and the main correlation
-    βs = [ log10(30); βs]
-    concs = [ 306*30^-2 / (1+ 306*30^-2); concs ]
+    # near-field location
+    xnf = 30
+    xmin = 10^(minimum(βs))
+    if xnf < xmin
+        # linear interpolation between the near-field correlation
+        # and the main correlation
+        βnf = log10(xnf)
+        cnf = 306*xnf^-2 / (1+ 306*xnf^-2)
+        βs = [ βnf; βs]
+        concs = [ cnf; concs ]
+    else
+        # for α > 0.605 the near-field overlaps with the correlation
+        # stop the near-field correlation early
+        # (this can lead to discontinuities)
+        xnf = xmin
+    end
 
-    # linear interpolation, extrapolates past the end with a straight line
-    itp = extrapolate(interpolate((βs,), concs, Gridded(Linear())), Line())
+    # linear interpolation
+    itp = interpolate((βs,), concs, Gridded(Linear()))
+
+    # far field correlation
+    # starts at last interpolation point and decays like x′^-2
+    xff = 10^(maximum(βs))
+    A = minimum(concs)*xff^2
 
     # plume dimension parameters
-    lb  = Vr*gₒ/(u₁₀^3)
+    lb  = Q*gₒ/(u₁₀^3)
 
     return BritterMcQuaidPlumeSolution(
         scenario, #scenario::Scenario
         :brittermcquaid, #model::Symbol
-        ρⱼ,    # jet_density
+        c₀,    # initial concentration, kg/m³
         T′,    # temperature_correction
         D,     # critical length, m
         lb,    # length parameter, m
-        itp    # interpolation::Extrapolation
+        itp,   # interpolation::Extrapolation
+        xnf,   # near-field distance
+        xff,   # far-field distance
+        A      # far-field constant
     )
 end
 
 function (b::BritterMcQuaidPlumeSolution)(x, y, z)
-    D = b.D
-    lb = b.lb
-    Lu  = 0.5*D + 2*lb
-    Lh0 = D + 8*lb
+
+    # plume dimension parameters
+    Lu  = 0.5*b.D + 2*b.lb
+    Lh0 = b.D + 8*b.lb
+
     # domain check
     if x < -Lu
         return 0.0
@@ -110,31 +218,34 @@ function (b::BritterMcQuaidPlumeSolution)(x, y, z)
 
     # plume dimensions
     if x < 0
-        # connecting the upwind extent to the crosswind extent at x=0
-        # using a similar curve as for x>0
-        x′ = x + Lu
-        A = (Lh0^3)/(Lu^2)
-        Lh = cbrt(A*x′^2)
-        Lv = (D^2)/Lh0
+        # Britter-McQuaid model does not give a profile for x<0
+        # assuming crosswind length is Lh0
+        Lh = Lh0
     else
-        Lh = Lh0 + 2.5*cbrt(lb*x^2)
-        Lv = (D^2)/Lh0
+        Lh = Lh0 + 2.5*cbrt(b.lb*x^2)
     end
 
+    Lv = (b.D^2)/Lh
     # within the extent of the plume
     if (y < -Lh) || (y > Lh) || (z < 0) || (z > Lv)
         return 0.0
     end
 
-    x′ = x/D
-    if x′ < 30
-        # use short distance correlation
+    x′ = x/b.D
+    if x′ ≤ b.xnf
+        # use near-field correlation
         c′ = x′ > 0 ? 306*x′^-2 / (1+ 306*x′^-2) : 1.0
-    else
+    elseif b.xnf < x′ < b.xff
         # use linear interpolation
         β = log10(x′)
         c′ = b.itp(β)
+    else
+        # use far-field correlation
+        # where A is a function of α
+        c′ = b.A/(x′^2)
     end
-    c = ( b.ρⱼ*c′*b.T′) / (1 - c′ + c′*b.T′)
+
+    # non-isothermal correction
+    c = b.c₀*c′ / (c′ + (1 - c′)*b.T′)
     return c
 end
--- test/models/britter_mcquaid_plume_tests.jl
+++ test/models/britter_mcquaid_plume_tests.jl
@@ -12,8 +12,8 @@
                   liquid_heat_capacity = 2.0564) # Methane, NIST Webbook
     r = Release( mass_rate = (0.23*425.6),
                  duration = 174,
-                 diameter = 0,
-                 velocity = 0,
+                 diameter = 1.0,
+                 velocity = 70.815,
                  height = 10.0,
                  pressure = 101325.0,
                  temperature = (273.15-162),
@@ -21,10 +21,14 @@
     a = DryAir(windspeed=10.9, temperature=298, stability=ClassF)
     scn = Scenario(s,r,a)
     # known answers
-    # set 1 is covers the short-distance correlation
-    # set 2 is the answer from the above example, which uses the interpolations
-    x₁, c₁ = 2.26*20, 1.1827612798486664
-    x₂, c₂ = 367.0, 0.08922951841378725
+    # initial concentration
+    c₀ = 1.760006673092362
+    # in the near-field
+    x₁, c₁ = 2.26*20, 1.1827642935170182
+    # example, in the interpolation region
+    x₂, c₂ = 367.0, 0.08925280058298538
+    # far field
+    x₃, c₃ = 1200.0, 0.008660197082795383
 
     # test type inheritance
     pl = plume(scn, BritterMcQuaidPlume)
@@ -32,17 +36,29 @@
     @test isa(pl, Plume)
     @test pl(x₁,0,0) ≈ c₁
     @test pl(x₂,0,0) ≈ c₂
+    @test pl(x₃,0,0) ≈ c₃
 
     # test plume extent
     Lu  = 0.5*pl.D + 2*pl.lb
     @test pl(-Lu - 2*eps(Float64), 0, 0) == 0.0
+    @test pl(-Lu + 2*eps(Float64), 0, 0) ≈ c₀
 
     Lh0 = pl.D + 8*pl.lb
     @test pl(0, Lh0 + 2*eps(Float64), 0) == 0.0
+    @test pl(0, Lh0 - 2*eps(Float64), 0) ≈ c₀
     @test pl(0 - 2*eps(Float64), Lh0 + 2*eps(Float64), 0) == 0.0
+    @test pl(0 - 2*eps(Float64), Lh0 - 2*eps(Float64), 0) ≈ c₀
 
     Lv = pl.D^2/Lh0
     @test pl(0, 0, -1) == 0.0
     @test pl(0, 0, Lv + 2*eps(Float64)) == 0.0
+    @test pl(0, 0, Lv - 2*eps(Float64)) ≈ c₀
+
+    # test large α near-field
+    a = DryAir(windspeed=0.8322, temperature=298, stability=ClassF)
+    scn = Scenario(s,r,a)
+    pl_nf = plume(scn, BritterMcQuaidPlume)
+    c_nf  = 0.9346808405962113
+    @test pl_nf(pl_nf.xnf*pl_nf.D,0,0) ≈ c_nf
 
 end
