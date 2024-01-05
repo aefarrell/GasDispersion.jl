de155811e58ee5cf4094bff8fd41a19bbb6e2856
allan <75338859+aefarrell@users.noreply.github.com>
2024-01-05 15:27:38 -0700

    Using ENV vars to pick the right tests
    Using ENV variables to run tests in parallel

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,8 @@ push!(LOAD_PATH,"../src/")
 
 using Documenter, GasDispersion
 
+ENV["GKSwstype"] = "100"
+
 makedocs(sitename="GasDispersion.jl Documentation",
          pages=["scenarios.md", "plume.md", "puff.md", "equation_sets.md", "references.md", "function_index.md"])
 
