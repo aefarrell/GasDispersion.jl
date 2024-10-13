ccdc8cc9649f59dc457a4a19548efafcb6b91e4c
allan <75338859+aefarrell@users.noreply.github.com>
2024-10-13 15:59:35 -0600

    adding VerticalJet behaviour to SimpleJet model
    just changes the default angle to point 90\deg up

--- test/models/simple_jet_tests.jl
+++ test/models/simple_jet_tests.jl
@@ -23,13 +23,13 @@
            dischargecoef = 0.85,
            temperature = T1, # K
            pressure = P1,    # Pa
-           height = 3.5)     # m, height of hole above the ground
+           height = 300)     # m, height of hole above the ground
     
     # known answers
     a, b = 6, 5
     ξ² = log(2)/b^2
-    x, y, z =  a, √(a^2*ξ²), 1
-    c = 0.00014697139683447435
+    x, y, z =  a, √(a^2*ξ²), 300
+    c = 0.011276270392345168
 
     # horizontal jet
     j = plume(scn, SimpleJet; release_angle=0, k2=a, k3=b)
@@ -37,5 +37,21 @@
     @test isa(j, Plume)
     @test j(x,y,z) ≈ c
     @test j(-x,y,z) == 0.0
+
+    # vertical jet
+    v = VerticalJet(; mass_rate=scn.release.ṁ,
+                      duration=scn.release.Δt,
+                      diameter=scn.release.d,
+                      velocity=scn.release.u,
+                      height=scn.release.h,
+                      pressure=scn.release.P,
+                      temperature=scn.release.T,
+                      fraction_liquid=scn.release.f_l)
+    scn2 = Scenario(scn.substance,v,scn.atmosphere)
+    j2 = plume(scn2, SimpleJet; k2=a, k3=b)
+    @test isa(j2, GasDispersion.SimpleJetSolution)
+    @test isa(j2, Plume)
+    @test j2(0,y,v.h+x) ≈ c
+    @test j2(0,y,v.h-eps(y)) == 0.0
 
 end
